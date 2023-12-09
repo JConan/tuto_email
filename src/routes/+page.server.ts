cc9164c3b7c680ec3ff826ca40dc61bd0af02005
JConan <conanjahh@hotmail.com>
2023-12-09 23:10:38 +0100

    update packages

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,37 +1,37 @@
-import { MAILBOX_LOGIN } from "$env/static/private";
-import { fail } from "@sveltejs/kit";
-import { EmailDataSchema, sendTestMail } from "$lib/server/Email";
-import { ZodError } from "zod";
+import { MAILBOX_LOGIN } from '$env/static/private';
+import { fail } from '@sveltejs/kit';
+import { EmailDataSchema, sendTestMail } from '$lib/server/Email';
+import { ZodError } from 'zod';
 
 export async function load() {
-    return {
-        contactAddress: MAILBOX_LOGIN
-    }
+	return { contactAddress: MAILBOX_LOGIN };
 }
 
 export const actions = {
-    default: async ({ request }) => {
-        const formData = await request.formData()
-        const contact = formData.get('contact')!.toString()
-        const title = formData.get('title')!.toString()
-        const content = formData.get('content')!.toString()
+	default: async ({ request }) => {
+		const formData = await request.formData();
+		const contact = formData.get('contact')!.toString();
+		const title = formData.get('title')!.toString();
+		const content = formData.get('content')!.toString();
 
-        try {
-            const emailData = EmailDataSchema.parse({
-                contact, title, content
-            })
-            await sendTestMail(emailData)
-            await new Promise(resolve => setTimeout(resolve, 1000))
-            return {
-                success: true
-            }
-        } catch (e) {
-            if (e instanceof ZodError) {
-                return fail(400, { success: false, errors: e.formErrors.fieldErrors })
-            } else {
-                console.dir(e, { depth: null })
-                return fail(500, { success: false, message: 'Internal error' })
-            }
-        }
-    }
-}
+		try {
+			const emailData = EmailDataSchema.parse({
+				contact,
+				title,
+				content
+			});
+			await sendTestMail(emailData);
+			await new Promise((resolve) => setTimeout(resolve, 1000));
+			return {
+				success: true
+			};
+		} catch (e) {
+			if (e instanceof ZodError) {
+				return fail(400, { success: false, errors: e.formErrors.fieldErrors });
+			} else {
+				console.dir(e, { depth: null });
+				return fail(500, { success: false, message: 'Internal error' });
+			}
+		}
+	}
+};
